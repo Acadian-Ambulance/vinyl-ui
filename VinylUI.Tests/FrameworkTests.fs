c427204c4c986f315fb731d9c2747d7585833701
Matthew Miller <Matthew.Miller@acadian.com>
2017-07-21 13:38:13 -0500

    Adds tests for Model module.

--- VinylUI.Tests/FrameworkTests.fs
+++ VinylUI.Tests/FrameworkTests.fs
@@ -5,12 +5,33 @@ open VinylUI
 open FsUnitTyped
 
 type Record = {
-    StringProperty: string
-    IntProperty: int
+    Text: string
+    Number: int
 }
+with
+    static member TextProperty = typedefof<Record>.GetProperty("Text")
+    static member NumberProperty = typedefof<Record>.GetProperty("Number")
 
 [<Test>]
-let ``permuteModel creates new model with new property value``() =
-    let before = { StringProperty = "before"; IntProperty = 7 }
-    let after = Model.permute before "StringProperty" "after"
-    after |> shouldEqual { StringProperty = "after"; IntProperty = 7 }
+let ``Model.changes detects single change``() =
+    let original = { Text = "before value"; Number = 1 }
+    let updated = { original with Number = 7 }
+    let changes = Model.changes original updated |> Seq.toList
+    changes |> shouldEqual [Record.NumberProperty, box 7]
+
+[<Test>]
+let ``Model.changes detects multiple changes``() =
+    let original = { Text = "before value"; Number = 1 }
+    let updated = { Text = "after value"; Number = 7 }
+    let changes = Model.changes original updated |> Seq.toList
+    changes |> shouldEqual [Record.TextProperty, box "after value"; Record.NumberProperty, box 7]
+
+[<Test>]
+let ``Model.permute creates new model with new property value``() =
+    let original = { Text = "before value"; Number = 1 }
+
+    let newString = Model.permute original "Text" "after value"
+    newString |> shouldEqual { original with Text = "after value" }
+
+    let newInt = Model.permute original "Number" 7
+    newInt |> shouldEqual { original with Number = 7 }
